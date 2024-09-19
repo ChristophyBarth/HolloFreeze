393dc0affb7dbb6c9e8b37ec51386e9ded7e427a
Christophy Barth <christopherbartholomewokeke@gmail.com>
2024-09-19 18:35:25 +0100

    Initial commit

---
+++ app/src/main/java/capps/learning/hollofreeze/MainActivity.kt
@@ -0,0 +1,15 @@
+package capps.learning.hollofreeze
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val fragmentTransaction = supportFragmentManager.beginTransaction()
+        fragmentTransaction.replace(R.id.fragment_container_view, OnboardingFragment())
+        fragmentTransaction.commit()
+    }
+}
