acfe417c6bd7da3724520dbf33305522061b9a4b
Christophy Barth <christopherbartholomewokeke@gmail.com>
2024-09-26 00:24:57 +0100

    Made a few changes to the validate user input function.

--- app/src/main/java/capps/learning/hollofreeze/CategoryAdapter.kt
+++ app/src/main/java/capps/learning/hollofreeze/CategoryAdapter.kt
@@ -1,10 +1,38 @@
 package capps.learning.hollofreeze
 
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
+import capps.learning.hollofreeze.databinding.CategoryItemBinding
 
-/*
-class CategoryAdapter : RecyclerView.Adapter<CategoryAdapter.CategoryViewHolder>() {
-    class CategoryViewHolder() : RecyclerView.ViewHolder(){
+class CategoryAdapter(private val context: Context, private val categories: List<Category>) :
+    RecyclerView.Adapter<CategoryAdapter.CategoryViewHolder>() {
 
+    inner class CategoryViewHolder(private val binding: CategoryItemBinding) :
+        RecyclerView.ViewHolder(binding.root) {
+        fun bind(category: Category) {
+            binding.categoryName.text = category.name
+            binding.categoryIcon.setImageResource(category.icon)
+
+            binding.card.setBackgroundColor(context.resources.getColor(category.color, null))
+        }
     }
-}*/
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): CategoryViewHolder {
+        val layoutInflater = LayoutInflater.from(parent.context)
+        val binding = CategoryItemBinding.inflate(layoutInflater, parent, false)
+        val categoryViewHolder = CategoryViewHolder(binding)
+
+        return categoryViewHolder
+    }
+
+    override fun getItemCount(): Int {
+        return categories.size
+    }
+
+    override fun onBindViewHolder(holder: CategoryViewHolder, position: Int) {
+        val category = categories[position]
+        holder.bind(category)
+    }
+}
--- app/src/main/java/capps/learning/hollofreeze/HomeFragment.kt
+++ app/src/main/java/capps/learning/hollofreeze/HomeFragment.kt
@@ -5,55 +5,39 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.recyclerview.widget.LinearLayoutManager
+import capps.learning.hollofreeze.databinding.FragmentHomeBinding
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
-
-/**
- * A simple [Fragment] subclass.
- * Use the [HomeFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
 class HomeFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
-    }
+    private lateinit var binding: FragmentHomeBinding
+    private lateinit var categoryAdapter: CategoryAdapter
+    private lateinit var listOfCategories : MutableList<Category>
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_home, container, false)
+    ): View {
+        binding = FragmentHomeBinding.inflate(layoutInflater)
+        return binding.root
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment HomeFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            HomeFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        val bagCategory = Category("Bags", R.drawable.ic_cart, R.color.ourPrimaryColor)
+        val shoeCategory = Category("Shoes", R.drawable.ic_profile, android.R.color.holo_blue_light)
+        val jewelryCategory = Category("Jewelries", R.drawable.ic_home, android.R.color.holo_orange_light)
+        val teeCategory = Category("Tees", R.drawable.ic_menu, android.R.color.holo_green_dark)
+
+        listOfCategories = mutableListOf()
+        listOfCategories.add(bagCategory)
+        listOfCategories.add(shoeCategory)
+        listOfCategories.add(jewelryCategory)
+        listOfCategories.add(teeCategory)
+
+        categoryAdapter = CategoryAdapter(requireContext(), listOfCategories)
+
+        binding.recyclerView.adapter = categoryAdapter
+        binding.recyclerView.layoutManager = LinearLayoutManager(requireContext(), LinearLayoutManager.HORIZONTAL, false)
     }
 }
